6dafd5204edd5b5815ef4d748f1d52d7a1c8cc83
Remi dumarais <remi75010@gmail.com>
2019-04-24 17:01:49 +0200

    modification de la classe adresse

--- src/com/metier/Adresse.java
+++ src/com/metier/Adresse.java
@@ -19,7 +19,7 @@ public class Adresse {
 	private String nomRue;
 	private String cp;
 	private String ville;
-	
+	//ma classe adresse
 	@OneToMany(mappedBy="adresse")
 	private List<Personne> personnes = new ArrayList<Personne>();
 	
